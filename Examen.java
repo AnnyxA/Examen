1abf4ab6ea44b58c121010c129597de715b440fe
AnnyxA <143622095+AnnyxA@users.noreply.github.com>
2023-09-20 08:58:34 -0600

    Add files via upload

---
+++ Examen.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Examen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int inst1, gan, pc; 
+                double pv1=1250.45, pv2=3743.00, pv3=2683.78;
+        
+        System.out.println ("Ingresa el nombre del instrumento");
+        
+System.out.println ("Ingresa el precio de compra en decimales");    
+canner: inst;
+gan=inst1/pc;
+System.out.println ("El porcentaje de ganancia de " + gan);
+    
+    }
+    
+}
